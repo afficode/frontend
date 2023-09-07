6950ba3b330fd03371ef735dc384cfa9b32ce172
Godstime Agholor <64644665+godstimedev@users.noreply.github.com>
2023-09-07 21:53:02 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,2 @@
-export { default as AboutUs } from './AboutUs';
-export { default as ContactUs } from './ContactUs';
 export { default as Home } from './Home';
 export { default as Welcome } from './Welcome';
