c264d49a5741501a300b733d7853cde09d169a39
schika1 <schika@cloudhight.com>
2024-02-01 19:02:50 +0000

    Merge

--- src/layout/Navbar/index.jsx
+++ src/layout/Navbar/index.jsx
@@ -732,7 +732,6 @@ const Navbar = () => {
 
                 <span className="border border-r-4 border-white h-[2rem]" />
 
-<<<<<<< HEAD
                 <li className="dropdown dropdown-hover">
                   <a
                     href={`${Approutes.product.category}/${btoa(50)}`}
@@ -754,23 +753,9 @@ const Navbar = () => {
                     />
                   )}
                 </li>
-=======
-								<li className="dropdown dropdown-hover">
-									<a href={`${Approutes.product.category}/${btoa(50)}`} tabIndex={0} className={listStyles}>
-										CARS & VEHICLES
-									</a>
-									<a href={`${Approutes.product.category}/${btoa(50)}`} className={mobileListStyles}>
-										<FaCarSide size={25} />
-									</a>
-									{filteredCategories?.vehicleCat && (
-										<Dropdown category={'CARS & VEHICLES'} subCategories={filteredCategories?.vehicleCat} />
-									)}
-								</li>
->>>>>>> df729ee13763ad614daf1e907722e35bf0911838
 
                 <span className="border border-r-4 border-white h-[2rem]" />
 
-<<<<<<< HEAD
                 <li className="dropdown dropdown-hover">
                   <a
                     href={`${Approutes.product.category}/${btoa(51)}`}
@@ -792,23 +777,9 @@ const Navbar = () => {
                     />
                   )}
                 </li>
-=======
-								<li className="dropdown dropdown-hover">
-									<a href={`${Approutes.product.category}/${btoa(51)}`} tabIndex={0} className={listStyles}>
-										PROPERTY
-									</a>
-									<a href={`${Approutes.product.category}/${btoa(51)}`} className={mobileListStyles}>
-										<FaBuilding size={25} />
-									</a>
-									{filteredCategories?.propertyCat && (
-										<Dropdown category={'PROPERTY'} subCategories={filteredCategories?.propertyCat} />
-									)}
-								</li>
->>>>>>> df729ee13763ad614daf1e907722e35bf0911838
 
                 <span className="border border-r-4 border-white h-[2rem]" />
 
-<<<<<<< HEAD
                 <li className="dropdown dropdown-hover ">
                   <a
                     href={`${Approutes.product.category}/${btoa(52)}`}
@@ -830,19 +801,6 @@ const Navbar = () => {
                     />
                   )}
                 </li>
-=======
-								<li className="dropdown dropdown-hover ">
-									<a href={`${Approutes.product.category}/${btoa(52)}`} tabIndex={0} className={listStyles}>
-										SERVICES
-									</a>
-									<a href={`${Approutes.product.category}/${btoa(52)}`} className={mobileListStyles}>
-										<MdMiscellaneousServices size={25} />
-									</a>
-									{filteredCategories?.servicesCat && (
-										<Dropdown category={'SERVICES'} subCategories={filteredCategories?.servicesCat} />
-									)}
-								</li>
->>>>>>> df729ee13763ad614daf1e907722e35bf0911838
 
                 <span className="border border-r-4 border-white h-[2rem]" />
 
