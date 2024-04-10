fc16e3572e4c1e8b3f7dd112a625745e8b9d2750
Godstime Agholor <agholorgodstime18@gmail.com>
2024-04-10 15:59:15 +0100

    fixed conflicts

--- src/pages/Grabs/Product/index.jsx
+++ src/pages/Grabs/Product/index.jsx
@@ -237,9 +237,9 @@ const GrabProduct = () => {
 				</div>
 			</div>
 
-			<div className="flex gap-6 my-8">
+			<div className="flex gap-6 my-8 max-sm:flex-col">
 				<Link to={Approutes.grab.flyer} className="flex-1">
-					<Button className={'bg-[#047F73] text-white px-8 w-full h-full font-semibold text-xl'}>
+					<Button className={'bg-[#047F73] text-white px-8 py-[.75rem] w-full  font-semibold text-xl'}>
 						Generate Post Now
 					</Button>
 				</Link>
