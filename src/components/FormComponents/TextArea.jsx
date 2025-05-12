499e100bc103f840031594c223302afcf7e61b34
Godstime Agholor <64644665+godstimedev@users.noreply.github.com>
2025-05-12 23:35:42 +0100

    Fix/ad posting (#100)
    This PR fixes https://github.com/afficode/frontend/issues/99

--- src/components/FormComponents/TextArea.jsx
+++ src/components/FormComponents/TextArea.jsx
@@ -1,49 +1,55 @@
-import { ErrorMessage, Field, useField, useFormikContext } from "formik";
-import TextError from "./TextError";
+import { ErrorMessage, Field, useField, useFormikContext } from 'formik';
+import TextError from './TextError';
+import { useState } from 'react';
 
 const TextArea = (props) => {
-  const { label, name, required, maxLength, ...rest } = props;
-  const [field] = useField(name);
-  const formik = useFormikContext();
+	const { label, name, required, maxLength, ...rest } = props;
+	const [field] = useField(name);
+	const formik = useFormikContext();
 
-  const handleChange = (event) => {
-    const inputValue = event.target.value;
+	const [wordCount, setWordCount] = useState(0);
 
-    // Enforce the text limit
-    if (maxLength && inputValue.length <= maxLength) {
-      formik.setFieldValue(name, inputValue);
-    }
-  };
-  return (
-    <div className="formControlClass">
-      {label && (
-        <label className="formLabel" htmlFor={name}>
-          {label}{" "}
-          {required ? (
-            <span className="text-black" title="This field is required">
-              *
-            </span>
-          ) : null}
-        </label>
-      )}
-      <Field
-        as="textarea"
-        name={name}
-        id={name}
-        cols={30}
-        rows={5}
-        onChange={handleChange}
-        value={field.value}
-        {...rest}
-      />
-      {maxLength && (
-        <p className="text-sm text-gray-500">
-          {field.value.length}/{maxLength} characters
-        </p>
-      )}
-      <ErrorMessage name={name} component={TextError} />
-    </div>
-  );
+	const handleChange = (event) => {
+		const inputValue = event.target.value;
+
+		const wordCount = inputValue.split(' ').filter((word) => word !== '').length;
+		setWordCount(wordCount);
+
+		// Enforce the text limit
+		if (maxLength && wordCount <= maxLength) {
+			formik.setFieldValue(name, inputValue);
+		}
+	};
+	return (
+		<div className="formControlClass">
+			{label && (
+				<label className="formLabel" htmlFor={name}>
+					{label}{' '}
+					{required ? (
+						<span className="text-black" title="This field is required">
+							*
+						</span>
+					) : null}
+				</label>
+			)}
+			<Field
+				as="textarea"
+				name={name}
+				id={name}
+				cols={30}
+				rows={5}
+				onChange={handleChange}
+				value={field.value}
+				{...rest}
+			/>
+			{maxLength && (
+				<p className="text-sm text-gray-500">
+					{wordCount}/{maxLength} words
+				</p>
+			)}
+			<ErrorMessage name={name} component={TextError} />
+		</div>
+	);
 };
 
 export default TextArea;
