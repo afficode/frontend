9a3f8dbf6b5995657eab0800fba7c9d924fcc1f8
godstime <agholorgodstime18@gmail.com>
2025-10-19 08:06:57 +0100

    refactor(message): refactored the message feature
    This commit includes;
    - new look chat window as designed on figma
    - used url params for chat state
    - mobile responsiveness update
    
    This closes: #142
    
    DCO 1.1 Signed-off-by Godstime Agholor agholorgodstime18@gmail.com

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 export * from './boonfuInfo';
 export * from './carouselSettings';
 export * from './formOptions';
+export * from './queryStrings';
 export * from './routes';
 export * from './CategoryFilterFormData';
--- src/pages/Profile/Messages/ChatWindow.jsx
+++ src/pages/Profile/Messages/ChatWindow.jsx
@@ -2,10 +2,8 @@ import { Link } from 'react-router-dom';
 import { noimage } from '../../../assets/images';
 import Message from './Message';
 import MessageInput from './MessageInput';
-
 import useAuth from '../../../context/UserContext';
-import { useEffect, useLayoutEffect, useRef } from 'react';
-import useMessageContext from '../../../context/MessageContext';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { useMessages } from '../../../hooks';
 import { encodeProductId, toMoney } from '../../../utils';
 import { Button } from '../../../ui';
@@ -13,10 +11,15 @@ import { Button } from '../../../ui';
 const ChatWindow = ({ chat_id, chat_data, title }) => {
 	const { user } = useAuth();
 
-	// console.log(user);
+	const [showContact, setShowContact] = useState(false);
 
-	const data = chat_data?.find((chat) => chat.chat_id === chat_id);
-	const { data: messageData } = useMessages(chat_id);
+	useEffect(() => {
+		setShowContact(false);
+	}, [chat_id]);
+
+	const data = chat_data?.find((chat) => chat.chat_id === Number(chat_id));
+
+	const { data: messageData } = useMessages(Number(chat_id));
 	// ref to scroll to latest message
 	const latestMessageRef = useRef(null);
 	useLayoutEffect(() => {
@@ -28,14 +31,24 @@ const ChatWindow = ({ chat_id, chat_data, title }) => {
 		}
 	}, [messageData]);
 
-	console.log(data);
+	const groupedMessages = messageData?.messages?.reduce((acc, message) => {
+		const dateKey = new Date(message.message_updated_on).toDateString();
+
+		if (!acc[dateKey]) {
+			acc[dateKey] = [];
+		}
+		acc[dateKey].push(message);
+		return acc;
+	}, {});
 
 	return (
-		<div className="bg-primary min-w-[380px] sm:w-full h-[calc(100vh-15rem)] overflow-x-auto rounded-xl pt-1 pb-4 px-1 flex flex-col  ">
+		<div className="bg-primary w-full sm:w-full h-[calc(100vh-15rem)]  rounded-xl pt-1 pb-4 px-1 flex flex-col  ">
 			{/* chat window header  */}
-			<h5 className="text-center p-2 text-base font-semibold text-white">{data?.user_b_name}</h5>
-			<div className="flex justify-between items-center w-full px-2 py-1 bg-gray-100 shadow-lg sm:py-2 rounded-xl">
-				<Link to={`/product/${encodeProductId(data?.ad_id)}`}>
+			<h5 className="text-center p-2 text-base font-semibold text-white">
+				{user?.id === data?.user_a ? data?.user_b_name : data?.user_a_name}
+			</h5>
+			<div className="flex gap-2 justify-between items-center flex-wrap w-full px-2 py-1 bg-gray-100 shadow-lg sm:py-2 rounded-xl">
+				<Link to={`/product/${encodeProductId(data?.ad_id)}`} className="max-w-max w-full">
 					<div className="flex gap-2 items-center">
 						<img
 							src={
@@ -44,38 +57,54 @@ const ChatWindow = ({ chat_id, chat_data, title }) => {
 							alt={data?.title || 'product'}
 							className="w-[3rem] h-[3rem]  object-fit rounded-full "
 						/>
-						<div className=" flex flex-col ">
-							<h6 className=" capitalize text-base leading-4">{data?.title ? data.title : title}</h6>
+						<div className=" flex flex-col w-full">
+							<h6 className=" capitalize text-base leading-4 break-all break-words">
+								{data?.title ? data.title : title}
+							</h6>
 							<h5 className="font-semibold text-sm">₦{toMoney(data?.price, true)}</h5>
 						</div>
 					</div>
 				</Link>
 
-				<Button
-					variant={'primary'}
-					size={'small'}
-					className={'!py-[0.125rem] px-[1rem] h-max leading-6 rounded-md text-sm'}
-				>
-					View contact
-				</Button>
+				{showContact ? (
+					<p className="text-primary pr-2 font-medium">
+						{user?.id === data?.user_a ? data?.user_b_phone : data?.user_a_phone}
+					</p>
+				) : (
+					<Button
+						onClick={() => setShowContact(true)}
+						variant={'primary'}
+						size={'small'}
+						className={'!py-[0.125rem] px-[1rem] h-max leading-6 rounded-md text-sm whitespace-nowrap'}
+					>
+						View contact
+					</Button>
+				)}
 			</div>
 
 			{/* chat window */}
 			<div className="mt-auto flex flex-col py-3 pr-3 overflow-x-hidden overflow-y-auto scrollbar-thin scrollbar-track-white scrollbar-thumb-rounded-md scrollbar-thumb-secondary scrollbar-track-rounded-md ">
-				{messageData?.messages.map((message, i) => (
-					<div
-						key={i}
-						className={`w-fit max-w-[80%] py-2 ${message.sender === user.id ? 'ml-auto' : ''}`}
-						ref={i === messageData.messages.length - 1 ? latestMessageRef : null}
-					>
-						<Message key={i} message={message} time={message.message_updated_on} />
-					</div>
-				))}
+				{groupedMessages &&
+					Object.entries(groupedMessages)?.map(([date, messages]) => (
+						<div key={date} className="py-2 w-full flex flex-col gap-2">
+							<span className="w-full text-center text-xs text-white ">{date}</span>
+
+							{messages?.map((message, i) => (
+								<div
+									key={i}
+									className={`w-fit max-w-[80%] py-2 ${message.sender === user.id ? 'ml-auto' : ''}`}
+									ref={i === messages.length - 1 ? latestMessageRef : null}
+								>
+									<Message key={i} message={message} time={message.message_updated_on} />
+								</div>
+							))}
+						</div>
+					))}
 			</div>
 
 			{/* chat input */}
 			<div className="ml-auto w-full sm:w-[70%] mt-3 ">
-				<MessageInput id={chat_id} />
+				<MessageInput id={Number(chat_id)} />
 			</div>
 		</div>
 	);
