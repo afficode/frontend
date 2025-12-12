a25fb9a503f7b5b6322538a589cff365e92d1c8a
godstime <agholorgodstime18@gmail.com>
2025-12-12 23:42:45 +0100

    fix(minor_bugs): Removed paystack payment method for ads
    Removed paystack and made boonfu wallet the default payment method for ad payments.
    
    contributes to #251
    
    DCO Signed-Off: Godstime Agholor agholorgodstime18@gmail.com

--- src/pages/Checkout/PaymentOption/index.jsx
+++ src/pages/Checkout/PaymentOption/index.jsx
@@ -3,13 +3,10 @@ import { Button } from '../../../ui';
 import { useEscrow, useNotify, useQuotedPay } from '../../../hooks';
 import { useLocation, useParams } from 'react-router-dom';
 import useAuth from '../../../context/UserContext';
-import { useState } from 'react';
 import { frontendLink } from '../../../constants';
 
 const PaymentOption = ({ result, quotePrice, orderId }) => {
 	const { mutate: pay, isLoading } = useEscrow();
-
-	const [paymentOption, setPaymentOption] = useState('paystack');
 
 	const { grabber_id, ad_id } = useParams();
 	const { user } = useAuth();
@@ -52,7 +49,7 @@ const PaymentOption = ({ result, quotePrice, orderId }) => {
 				grabber_id: grabber_id,
 				ad_id: ad_id,
 				user_id: user?.id,
-				payment_method: paymentOption === 'paystack' ? 'paystack' : 'wallet',
+				payment_method: 'wallet',
 				stage: 'init',
 				callback_url: 'https://boonfu.site/checkout/payment-success',
 				// callback_url: 'http://localhost:5173/checkout/payment-success',
@@ -60,9 +57,9 @@ const PaymentOption = ({ result, quotePrice, orderId }) => {
 			},
 			{
 				onSuccess: (data) => {
-					// console.log(data);
+					console.log(data);
 					// notify('Please pay', 'success');
-					window.location.replace(data?.url);
+					// window.location.replace(data?.url);
 					// setStage(2);
 				},
 				onError: (error) => {
@@ -74,126 +71,50 @@ const PaymentOption = ({ result, quotePrice, orderId }) => {
 
 	return (
 		<section className="border border-black">
-			<h5 className="py-2 px-4 font-bold bg-gray-300">Payment options (select)</h5>
+			<h5 className="py-2 px-4 font-bold bg-gray-300">Payment</h5>
 
 			<div className="px-4 py-6 bg-white">
 				{/* select payment option  */}
-				<div className="flex justify-between ">
-					{escrow_type === 'pickup' && (
-						<div className="flex  gap-2">
-							<input
-								type="radio"
-								name={'paystack'}
-								id={'paystack'}
-								value={'paystack'}
-								checked={paymentOption === 'paystack'}
-								disabled
-								onChange={(e) => setPaymentOption(e.target.value)}
-								className={`mt-1 cursor-pointer`}
-							/>
-							<label htmlFor="paystack" className=" cursor-pointer">
-								Paystack
-								{/* <div className="flex gap-2 items-center">
-								<img src={Visa} alt="/" className="w-8" />
-								<img src={Mastercard} alt="/" className="w-6" />
-							</div> */}
-							</label>
-						</div>
-					)}
+				<p className="text-sm italic mb-4">
+					Note: This payment will be deducted from your Boonfu wallet. Please ensure you have sufficient
+					balance.
+				</p>
 
-					<div className="flex  gap-2">
-						<input
-							type="radio"
-							name={'boonfu_wallet'}
-							id="boonfu_wallet"
-							value={'boonfu_wallet'}
-							checked={paymentOption === 'boonfu_wallet'}
-							onChange={(e) => setPaymentOption(e.target.value)}
-							className={`mt-1 cursor-pointer`}
-						/>
-						<label htmlFor="boonfu_wallet" className="cursor-pointer">
-							Boonfu wallet
-						</label>
+				<div className=" mt-6 border-2 border-primary  !px-2 !py-4 h-fit space-y-6 max-w-[400px] mx-auto mb-4">
+					<table className="w-full ">
+						<tbody className="border-b border-black/30">
+							<tr>
+								<td>Item</td>
+								<td>{toMoney(result?.data?.price)}</td>
+							</tr>
+							{quotePrice > 0 && (
+								<tr>
+									<td>Quote</td>
+									<td>{toMoney(quotePrice)}</td>
+								</tr>
+							)}
+						</tbody>
+						<tfoot>
+							<tr>
+								<td>Order Total</td>
+								<td>{toMoney(total)}</td>
+							</tr>
+						</tfoot>
+					</table>
+					<div>
+						<Button
+							type="button"
+							onClick={escrow_type === 'pickup' ? handlePickupPay : handlePay}
+							variant={'primary'}
+							size={'full'}
+							className={'rounded-2xl font-bold text-xl'}
+							loading={escrowLoading || quoteLoading || isLoading}
+							disabled={escrowLoading || quoteLoading || isLoading}
+						>
+							Confirm and pay
+						</Button>
 					</div>
 				</div>
-
-				{/* paystack option ui */}
-				{paymentOption === 'paystack' && (
-					<div className=" mt-6 border-2 border-primary  !px-2 !py-4 h-fit space-y-6 max-w-[400px] mx-auto mb-4">
-						<table className="w-full ">
-							<tbody className="border-b border-black/30">
-								<tr>
-									<td>Item</td>
-									<td>{toMoney(result?.data?.price)}</td>
-								</tr>
-								{quotePrice > 0 && (
-									<tr>
-										<td>Quote</td>
-										<td>{toMoney(quotePrice)}</td>
-									</tr>
-								)}
-							</tbody>
-							<tfoot>
-								<tr>
-									<td>Order Total</td>
-									<td>{toMoney(total)}</td>
-								</tr>
-							</tfoot>
-						</table>
-						<div>
-							<Button
-								type="button"
-								onClick={escrow_type === 'pickup' ? handlePickupPay : handlePay}
-								variant={'primary'}
-								size={'full'}
-								className={'rounded-2xl font-bold text-xl'}
-								loading={escrowLoading || quoteLoading || isLoading}
-								disabled={escrowLoading || quoteLoading || isLoading}
-							>
-								Confirm and pay
-							</Button>
-						</div>
-					</div>
-				)}
-
-				{/* boonfu wallet option ui */}
-				{paymentOption === 'boonfu_wallet' && (
-					<div className=" mt-6 border-2 border-primary  !px-2 !py-4 h-fit space-y-6 max-w-[400px] mx-auto mb-4">
-						<table className="w-full ">
-							<tbody className="border-b border-black/30">
-								<tr>
-									<td>Item</td>
-									<td>{toMoney(result?.data?.price)}</td>
-								</tr>
-								{quotePrice > 0 && (
-									<tr>
-										<td>Quote</td>
-										<td>{toMoney(quotePrice)}</td>
-									</tr>
-								)}
-							</tbody>
-							<tfoot>
-								<tr>
-									<td>Order Total</td>
-									<td>{toMoney(total)}</td>
-								</tr>
-							</tfoot>
-						</table>
-						<div>
-							<Button
-								type="button"
-								onClick={escrow_type === 'pickup' ? handlePickupPay : handlePay}
-								variant={'primary'}
-								size={'full'}
-								className={'rounded-2xl font-bold text-xl'}
-								loading={escrowLoading || quoteLoading || isLoading}
-								disabled={escrowLoading || quoteLoading || isLoading}
-							>
-								Confirm and pay
-							</Button>
-						</div>
-					</div>
-				)}
 			</div>
 		</section>
 	);
