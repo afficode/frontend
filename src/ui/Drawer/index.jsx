ba34a87031fdf7d17d79496687dbc95e1bfb0a4e
godstime <agholorgodstime18@gmail.com>
2025-12-14 23:16:37 +0100

    Revert "fix(filter_ui): fixed filter ui"
    This reverts commit 569ebba86acae7523cada8d63f41783e1c29db66.

--- src/ui/Drawer/index.jsx
+++ src/ui/Drawer/index.jsx
@@ -1,6 +1,6 @@
 const Drawer = ({ items, icon }) => {
 	return (
-		<div className="drawer">
+		<div className="drawer z-[2000] ">
 			<input id="my-drawer" type="checkbox" className="drawer-toggle" />
 			<div className="drawer-content ">
 				{/* Page content here */}
@@ -14,12 +14,8 @@ const Drawer = ({ items, icon }) => {
 					</label>
 				)}
 			</div>
-			<div className="drawer-side z-[2000] min-h-screen">
-				<label
-					htmlFor="my-drawer"
-					aria-label="close sidebar"
-					className="drawer-overlay min-h-full"
-				></label>
+			<div className="drawer-side ">
+				<label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
 				<ul className="menu p-4 w-80 min-h-full bg-white ">
 					{/* Sidebar content here */}
 					{items}
