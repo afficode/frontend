d501a707cd6ef176f1957065b9394d0ccaa9e89e
godstime <agholorgodstime18@gmail.com>
2026-03-11 13:22:37 +0100

    chore(postad): implement post ad category modal for mobile
    this commit implements the post ad category modal for mobile devices when the post ad button is clicked
    also includes adding navigation to grab home page when join now is clicked in AccountHistory component
    
    DCO Signed Off: Godstime Agholor <agholorgodstime18@gmail.com>

---
+++ src/components/PostAdModal/index.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom"
+import { Modal } from "../../ui"
+import { Approutes } from "../../constants"
+
+const PostAdModal = ({modalOpen, setModalOpen, filteredCategories}) => {
+  return (
+        <Modal
+            isOpen={modalOpen}
+            setIsOpen={setModalOpen}
+            padding={false}
+            className={'px-4 !py-2'}
+            modalHeader={false}
+        >
+            <div className="flex flex-col gap-4 items-center">
+                <h3 className="text-xl font-semibold">Post Ad in</h3>
+                <ul className='flex flex-col gap-1 w-full'>
+                    {filteredCategories?.allCat
+                        ?.slice(0, 14)
+                        ?.map((category) => (
+                            <Link
+                                to={`${Approutes.postAd}/${category.id}`}
+                                key={category.id}
+                                onClick={() => setModalOpen(false)}
+                            >
+                                <li className='capitalize text-base hover:text-secondary active:text-secondary '>
+                                    {category.name}
+                                </li>
+                            </Link>
+                        ))}
+                </ul>
+            </div>
+        </Modal>
+  )
+}
+
+export default PostAdModal
